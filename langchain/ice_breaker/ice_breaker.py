bb20d434d1f0a38fe8b53142dd9b5a5713652928
Subham Paul <subhampaul12@gmail.com>
2025-05-18 10:42:32 +0000

    completed ice creaker mini project by integrating tools like tavily and agent like react

--- langchain/ice_breaker/ice_breaker.py
+++ langchain/ice_breaker/ice_breaker.py
@@ -1,16 +1,19 @@
 import os
 from dotenv import load_dotenv
 from langchain.prompts import PromptTemplate
+from agents.linkedin_lookup_agent import lookup as linkedin_lookup_agent
 from third_parties.linkedin import scrape_linkedin_profile
 from langchain_core.output_parsers import StrOutputParser
 from langchain_groq import ChatGroq
 
-if __name__ == "__main__":
-  load_dotenv()
+
+def ice_break_with(name: str):
   if not os.getenv("GROQ_API_KEY"):
     os.environ["GROQ_API_KEY"] = input("Enter API key for Groq: ")
 
-  print("Hello LangChain!")
+  linkedin_profile_url = linkedin_lookup_agent(name=name)
+  linkedin_data = scrape_linkedin_profile(
+      linkedin_profile_url=linkedin_profile_url)
 
   summary_template = """
   given the linkedin information {information} about a person from I want you to create:
@@ -26,9 +29,13 @@ if __name__ == "__main__":
   llm = ChatGroq(model="meta-llama/llama-4-scout-17b-16e-instruct",
                  temperature=0.7)
 
-chain = summary_prompt_template | llm | StrOutputParser()
-linkedin_data = scrape_linkedin_profile(
-    linkedin_profile_url="https://www.linkedin.com/in/subham-paul-079795142/",
-    mock=True)
-response = chain.invoke({"information": linkedin_data})
-print(response)
+  chain = summary_prompt_template | llm | StrOutputParser()
+  response = chain.invoke({"information": linkedin_data})
+  print(response)
+
+
+if __name__ == "__main__":
+  load_dotenv()
+
+  print("Ice Breaker Enter: ")
+  ice_break_with(name="Subham Paul Bell Integration India")
